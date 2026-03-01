8edba8179489d7648cfb14b57fadb411b0a770ee
Pevatr <Pevatr@seznam.cz>
2026-03-01 12:04:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
-const CACHE = 'habitos-v3';
+const CACHE = 'habitos-v1';
+const FILES = ['./index.html', './manifest.json', './icon.svg'];
+
 self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
